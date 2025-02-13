c2e4ba2cca83dcd179e34f44756fa4dee57caad0
Felix Reichenbach <38431269+felixreichenbach@users.noreply.github.com>
2025-02-13 22:35:56 +0100

    a first start

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,22 @@
+import type { Session } from 'svelte-kit-cookie-session';
+
+type SessionData = {
+	verifier: string;
+	accessToken?: string;
+	user?: any;
+};
+
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
+		interface Locals {
+			session: Session<SessionData>;
+		}
+		interface PageData {
+			session: SessionData;
+		}
 		// interface PageState {}
 		// interface Platform {}
 	}
---
+++ src/routes/api/auth/+server.ts
@@ -0,0 +1,42 @@
+import type { RequestHandler } from '@sveltejs/kit';
+import { FusionAuthClient } from '@fusionauth/typescript-client';
+import { VITE_CLIENT_ID, VITE_CLIENT_SECRET, VITE_BASE_URL } from '$env/static/private';
+
+const client = new FusionAuthClient('noapikeyneeded', VITE_BASE_URL);
+
+export const GET: RequestHandler = async ({ url, locals }) => {
+	const code = url.searchParams.get('code');
+	const verifier = locals.session.data.verifier;
+
+	if (!code || !verifier) {
+		return new Response('Missing code or verifier', { status: 400 });
+	}
+	try {
+		const response = await client.exchangeOAuthCodeForAccessTokenUsingPKCE(
+			code,
+			VITE_CLIENT_ID,
+			VITE_CLIENT_SECRET,
+			'http://localhost:3000/auth',
+			verifier
+		);
+
+		const accessToken = response.response.access_token;
+		if (!accessToken) {
+			return new Response('Failed to retrieve access token', { status: 500 });
+		}
+		locals.session.data.accessToken = accessToken;
+
+		const userResponse = await client.retrieveUserUsingJWT(accessToken);
+		locals.session.data.user = userResponse.response.user;
+
+		return new Response(null, {
+			status: 302,
+			headers: {
+				location: '/'
+			}
+		});
+	} catch (err) {
+		console.error(err);
+		return new Response('Error exchanging OAuth code for access token', { status: 500 });
+	}
+};
