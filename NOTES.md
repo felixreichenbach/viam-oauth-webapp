6fec3717e1d2c1c40d686f00699f4fe5c53cbc0c
Felix Reichenbach <38431269+felixreichenbach@users.noreply.github.com>
2025-02-14 20:04:42 +0100

    First Working Version (#1)

---
+++ NOTES.md
@@ -0,0 +1,7 @@
+# Additional Notes
+
+Add Session Mgmt.
+https://www.npmjs.com/package/svelte-kit-cookie-session
+
+Seems to be server APIs
+https://svelte.dev/docs/kit/routing#server
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	const CLIENT_ID = import.meta.env.VITE_FUSION_AUTH_CLIENT_ID;
+	const LOGIN_CALLBACK = 'http://localhost:5173/callback';
+
+	const href =
+		'https://auth.viam.com/oauth2/authorize?client_id=' +
+		CLIENT_ID +
+		'&response_type=code&redirect_uri=' +
+		encodeURI(LOGIN_CALLBACK);
+</script>
+
+<h1>Welcome to Viam OAuth</h1>
+<a {href}>Login</a>
